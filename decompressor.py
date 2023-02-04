c94d1b0f35ef29b86b01b7023c77ec126889c763
Laiho <emillaiho@hotmail.fi>
2023-02-04 21:44:45 +0200

    decode huffman codelens

--- decompressor.py
+++ decompressor.py
@@ -8,6 +8,10 @@ import numpy as np
 
 class Huffman:
     def __init__(self, codelengths):
+        # Create codes that are made in a way that no key can be 
+        # a sub-key of another ie. this is impossible:
+        # key1 = 000111   key2 = 0001
+
         self.bits_to_symbol = {}
         max_codelength = max(codelengths) + 1
         nextcode = 0
@@ -19,6 +23,14 @@ class Huffman:
                 if codelen == codelength:
                     self.bits_to_symbol[startbit + nextcode] = idx
                     nextcode += 1
+
+    def interpert_next_symbol(self, bitreader):
+        # Read bits until we find a match in our dictionary
+        bits = 1
+        while True:
+            bits = bits << 1 | bitreader.read_bit()
+            if bits in self.bits_to_symbol:
+                return self.bits_to_symbol[bits]
 
 
 class Decompressor:
@@ -42,11 +54,36 @@ class Decompressor:
             code_len_list[j] = self.bitreader.read_n_bit_int(3)
         return code_len_list
 
+    def generate_codelengths(self, h, n_literal_codes, n_distcodes):
+        codes = []
+        while len(codes) < n_literal_codes + n_distcodes:
+            symbol = h.interpert_next_symbol(self.bitreader)
+            # 0-15 literal
+            if symbol <= 15:
+                codes.append(symbol)
+            # Copy previous code 3-6 times
+            if symbol == 16:
+                extra_runs = self.bitreader.read_n_bit_int(2)
+                for _ in range(3 + extra_runs):
+                    codes.append(codes[-1])
+            # Repeat a code len of 0 for 3-10 times
+            if symbol == 17:
+                extra_runs = self.bitreader.read_n_bit_int(3)
+                for _ in range(3 + extra_runs):
+                    codes.append(0)                
+            # Repeat a code len of 0 for 11-138 times
+            if symbol == 18:
+                extra_runs = self.bitreader.read_n_bit_int(7)
+                for _ in range(3 + extra_runs):
+                    codes.append(0)                
+        return codes
+
     def decode_huffman_tree(self):
-        print(self.bitreader.bit_idx)
-        n_lit_codes = self.bitreader.read_n_bit_int(5) + 257
+        n_literal_codes = self.bitreader.read_n_bit_int(5) + 257
         n_distcodes = self.bitreader.read_n_bit_int(5) + 1
-        cla = self.generate_codelen_arr()
+        codelen_arr = self.generate_codelen_arr()
+        h = Huffman(codelen_arr)
+        codes = self.generate_codelengths(h, n_literal_codes, n_distcodes)
 
     def decompress(self):
         is_last = self.bitreader.read_bit()
--- tests/test_decompressor.py
+++ tests/test_decompressor.py
@@ -5,6 +5,10 @@ from decompressor import Huffman
 
 CODELEN_ARR_STARTBIT = 13
 LIT_DIST_STARTBIT = 3
+SYMBOL_DECODE_STARTBIT = 65
+N_LITERAL_CODES = 269
+N_DISTCODES = 13
+
 
 class TestDecompressor(unittest.TestCase):
     def setUp(self):
@@ -19,12 +23,42 @@ class TestDecompressor(unittest.TestCase):
 
     def test_lit_and_dist_codes(self):
         self.decompressor.bitreader.bit_idx = CODELEN_ARR_STARTBIT
-        self.assertEqual(self.decompressor.bitreader.read_n_bit_int(5) + 257, 269)
-        self.assertEqual(self.decompressor.bitreader.read_n_bit_int(5) + 1, 13)
+        self.assertEqual(self.decompressor.bitreader.read_n_bit_int(5) + 257, N_LITERAL_CODES)
+        self.assertEqual(self.decompressor.bitreader.read_n_bit_int(5) + 1, N_DISTCODES)
 
     def test_huffman_creator(self):
         codes = [2, 0, 4, 3, 3, 4, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4]
         h = Huffman(codes)
         self.assertEqual(h.bits_to_symbol, {4: 0, 10: 3, 11: 4, 12: 6, 13: 17, 28: 2, 29: 5, 30: 7, 31: 18})
 
+    def test_decode_symbol(self):
+        self.decompressor.bitreader.bit_idx = SYMBOL_DECODE_STARTBIT
+        codes = [2, 0, 4, 3, 3, 4, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4]
+        h = Huffman(codes)
+        self.assertEqual(h.interpert_next_symbol(self.decompressor.bitreader), 17)
+        self.assertEqual(h.interpert_next_symbol(self.decompressor.bitreader), 18)
+        self.assertEqual(h.interpert_next_symbol(self.decompressor.bitreader), 5)
 
+    def test_dyn_huffman_decode(self):
+        self.decompressor.bitreader.bit_idx = SYMBOL_DECODE_STARTBIT
+        codes = [2, 0, 4, 3, 3, 4, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4]
+        codes = self.decompressor.generate_codelengths(Huffman(codes), N_LITERAL_CODES, N_DISTCODES)
+        # Ehh
+        self.assertEqual(codes, 
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 7, 0, 5, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0,
+            0, 0, 7, 0, 0, 0, 0, 0, 0, 6, 0, 0, 6, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 4, 0, 0, 5, 4, 0, 7, 4, 5, 0, 0, 6,
+            0, 4, 4, 0, 0, 6, 4, 3, 6, 6, 6, 0, 6, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 6, 5, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0,
+            0, 3, 0, 0, 0, 3, 0, 2, 3, 2, 0, 3, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 4, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
